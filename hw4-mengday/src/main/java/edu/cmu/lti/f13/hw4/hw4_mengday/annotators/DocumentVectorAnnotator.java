a5060794adc1414897564dd52f95ba703389a71b
Mengda Y <mengda.yang@gmail.com>
2013-10-24 19:26:21 -0400

    Finished First 2 Annotators coding!

--- hw4-mengday/src/main/java/edu/cmu/lti/f13/hw4/hw4_mengday/annotators/DocumentVectorAnnotator.java
+++ hw4-mengday/src/main/java/edu/cmu/lti/f13/hw4/hw4_mengday/annotators/DocumentVectorAnnotator.java
@@ -1,41 +1,67 @@
 package edu.cmu.lti.f13.hw4.hw4_mengday.annotators;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.cas.FSIterator;
+//import org.apache.uima.examples.tokenizer.Token;
 import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.cas.FSList;
 import org.apache.uima.jcas.cas.IntegerArray;
 import org.apache.uima.jcas.cas.StringArray;
 import org.apache.uima.jcas.tcas.Annotation;
 
 import edu.cmu.lti.f13.hw4.hw4_mengday.typesystems.Document;
+import edu.cmu.lti.f13.hw4.hw4_mengday.typesystems.Token;
+import edu.cmu.lti.f13.hw4.hw4_mengday.utils.Utils;
 
 public class DocumentVectorAnnotator extends JCasAnnotator_ImplBase {
 
-	@Override
-	public void process(JCas jcas) throws AnalysisEngineProcessException {
+  @Override
+  public void process(JCas jcas) throws AnalysisEngineProcessException {
 
-		FSIterator<Annotation> iter = jcas.getAnnotationIndex().iterator();
-		if (iter.isValid()) {
-			iter.moveToNext();
-			Document doc = (Document) iter.get();
-			createTermFreqVector(jcas, doc);
-		}
+    FSIterator<Annotation> iter = jcas.getAnnotationIndex(Document.type).iterator();
+    while (iter.hasNext()) {
+      //iter.moveToNext();
+      Document doc = (Document) iter.next();
+      createTermFreqVector(jcas, doc);
+    }
 
-	}
-	/**
-	 * 
-	 * @param jcas
-	 * @param doc
-	 */
+  }
 
-	private void createTermFreqVector(JCas jcas, Document doc) {
+  /**
+   * 
+   * @param jcas
+   * @param doc
+   */
 
-		String docText = doc.getText();
-		
-		//TO DO: construct a vector of tokens and update the tokenList in CAS
-		
+  private void createTermFreqVector(JCas jcas, Document doc) {
 
-	}
+    String docText = doc.getText();
+
+    // TO DO: construct a vector of tokens and update the tokenList in CAS
+
+    String[] wordList = docText.split(" ");
+    HashMap<String, Integer> tokenCount = new HashMap<String, Integer>();
+    for (String word : wordList) {
+      if (!tokenCount.containsKey(word)) {
+        tokenCount.put(word, 1);
+      } else {
+        tokenCount.put(word, tokenCount.get(word) + 1);
+      }
+    }
+
+    ArrayList<Token> tokenList = new ArrayList<Token>();
+    for (String word : tokenCount.keySet()) {
+      Token token = new Token(jcas);
+      token.setText(word);
+      token.setFrequency(tokenCount.get(word));
+      tokenList.add(token);
+    }
+    FSList tokenFSList = Utils.fromCollectionToFSList(jcas, tokenList);
+    doc.setTokenList(tokenFSList);
+  }
 
 }
--- hw4-mengday/src/main/java/edu/cmu/lti/f13/hw4/hw4_mengday/collectionreaders/DocumentReader.java
+++ hw4-mengday/src/main/java/edu/cmu/lti/f13/hw4/hw4_mengday/collectionreaders/DocumentReader.java
@@ -1,6 +1,8 @@
 package edu.cmu.lti.f13.hw4.hw4_mengday.collectionreaders;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
@@ -8,60 +10,71 @@ import org.apache.uima.jcas.JCas;
 
 import edu.cmu.lti.f13.hw4.hw4_mengday.typesystems.Document;
 
+public class DocumentReader extends JCasAnnotator_ImplBase {
 
-public class DocumentReader 
-extends JCasAnnotator_ImplBase  {
-	
-	@Override
-	public void process(JCas jcas) 
-			throws AnalysisEngineProcessException {
-		
-		// reading sentence from the CAS 
-		String sLine = jcas.getDocumentText();
+  private Pattern linePattern = Pattern.compile(".*?(\\r|\\n|\\r\\n)");
+  
+  @Override
+  public void process(JCas jcas) throws AnalysisEngineProcessException {
 
-		// TODO: make sure information from text collection are extracted correctly
-		ArrayList<String> docInfo = parseDataLine(sLine);
-		
-		//This is to make sure that parsing done properly and 
-		//minimal data for rel,qid,text are available to proceed 
-		if(docInfo.size()<3){
-			System.err.println("Not enough information in the line");
-			return;
-		}
-		int rel = Integer.parseInt(docInfo.get(0));
-		int qid = Integer.parseInt(docInfo.get(1));
-		String txt = docInfo.get(2);
-		
-		Document doc = new Document(jcas);
-		doc.setText(txt);
-		doc.setQueryID(qid);
-		//Setting relevance value
-		doc.setRelevanceValue(rel);
-		doc.addToIndexes();
-		
-		//Adding populated FeatureStructure to CAS
-		jcas.addFsToIndexes(doc);
-	}
+    // reading sentence from the CAS
+    String Document = jcas.getDocumentText();
+    
+    int pos = 0;
+    Matcher matcher = linePattern.matcher(Document);
+    
+    while(matcher.find(pos)){
+      pos = matcher.end();
+      String sLine = matcher.group();
+      // String sLine = jcas.getDocumentText();
 
+      // TODO: make sure information from text collection are extracted correctly
+      ArrayList<String> docInfo = parseDataLine(sLine);
 
-	public static ArrayList<String> parseDataLine(String line) {
-		ArrayList<String> docInfo;
+      // This is to make sure that parsing done properly and
+      // minimal data for rel,qid,text are available to proceed
+      if (docInfo.size() < 3) {
+        System.err.println("Not enough information in the line");
+        return;
+      }
 
-		String [] rec  = line.split("[\\t]");
-		String    sResQid = (rec[0]).replace("qid=", "");
-		String    sResRel = (rec[1]).replace("rel=", "");
-		
+      int rel = Integer.parseInt(docInfo.get(0));
+      int qid = Integer.parseInt(docInfo.get(1));
+      String txt = docInfo.get(2);
 
-		StringBuffer sResTxt = new StringBuffer();
-		for (int i=2; i<rec.length; i++) {
-			sResTxt.append(rec[i]).append(" ");					
-		}
+      Document doc = new Document(jcas);
+      doc.setText(txt);
+      doc.setQueryID(qid);
+      // Setting relevance value
+      doc.setRelevanceValue(rel);
+      doc.addToIndexes();
+      doc.setBegin(matcher.start());
+      doc.setEnd(pos);
 
-		docInfo = new ArrayList<String>();
-		docInfo.add(sResRel);
-		docInfo.add(sResQid);
-		docInfo.add(sResTxt.toString());
-		return docInfo;
-	}
+      // Adding populated FeatureStructure to CAS
+      jcas.addFsToIndexes(doc);
+    }
+    
+    System.out.println("Reader END!!!");
+  }
+
+  public static ArrayList<String> parseDataLine(String line) {
+    ArrayList<String> docInfo;
+
+    String[] rec = line.split("[\\t]");
+    String sResQid = (rec[0]).replace("qid=", "");
+    String sResRel = (rec[1]).replace("rel=", "");
+
+    StringBuffer sResTxt = new StringBuffer();
+    for (int i = 2; i < rec.length; i++) {
+      sResTxt.append(rec[i]).append(" ");
+    }
+
+    docInfo = new ArrayList<String>();
+    docInfo.add(sResRel);
+    docInfo.add(sResQid);
+    docInfo.add(sResTxt.toString());
+    return docInfo;
+  }
 
 }
